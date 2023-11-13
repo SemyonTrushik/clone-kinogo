a6fa1b4d3fd314f8bf67c1ec108f189f44165b50
Semyon_Trushik <semyontrushik@gmail.com>
2023-11-13 19:55:29 +0200

    add husky

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>  
+  <div class="main">
+    <hello-world/>
+  </div>
 </template>
 
 <script>
